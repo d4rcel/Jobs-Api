6790012df5c243a7ccecc13a6d940b3ec1dc32f6
d4rcel <daraola05@gmail.com>
2023-05-26 07:01:13 +0100

    Add more security for password

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,12 +1,18 @@
+const UserModel = require("../models/User.model");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError } = require("../errors");
+const bcrypt = require("bcryptjs");
+
 const register = async (req, res) => {
-    res.send('Register user')
-}
+  const user = await UserModel.create({ ...req.body });
+  res.status(StatusCodes.CREATED).json({ user });
+};
 
 const login = async (req, res) => {
-    res.send('Login user')
-}
-
+  res.send("Login user");
+};
 
 module.exports = {
-    register, login
-}
+  register,
+  login,
+};
--- models/User.model.js
+++ models/User.model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -22,8 +23,12 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: [true, "Please, provide password"],
     minlength: 6,
-    maxlength: 12,
   },
 });
 
-module.exports = mongoose.model("UserModel", UserSchema);
+UserSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+module.exports = mongoose.model("User", UserSchema);
