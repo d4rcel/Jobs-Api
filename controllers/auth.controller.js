f42e4ac5eb8d5a0bbd1cbec7c5b02a0dfe8e8f02
d4rcel <daraola05@gmail.com>
2023-05-26 07:54:56 +0100

    Register api

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,12 +1,15 @@
 const UserModel = require("../models/User.model");
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestError } = require("../errors");
-const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
   const user = await UserModel.create({ ...req.body });
-  res.status(StatusCodes.CREATED).json({ user });
+  const token = user.createJWT();
+
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
+
 
 const login = async (req, res) => {
   res.send("Login user");
